311db79e0316526674c729e35c1782934e88201b
Angelito Sardez, Jr <jojosardez@gmail.com>
2019-03-16 20:49:38 +1100

    Used YesNo pipe and update table

--- src/app/layout/administrators/administrators.module.ts
+++ src/app/layout/administrators/administrators.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AdministratorsRoutingModule } from './administrators-routing.module';
 import { AdministratorsComponent } from './administrators.component';
-import { PageHeaderModule } from './../../shared';
+import { PageHeaderModule, SharedPipesModule } from './../../shared';
 
 @NgModule({
-    imports: [CommonModule, AdministratorsRoutingModule, PageHeaderModule],
+    imports: [CommonModule, AdministratorsRoutingModule, PageHeaderModule, SharedPipesModule],
     declarations: [AdministratorsComponent]
 })
 export class AdministratorsModule {}
