df4c6ee36c3a6e8e244dc8f5e7ff4fb7ded37275
Angelito Sardez, Jr <jojosardez@gmail.com>
2019-03-23 23:29:58 +1100

    Moved outcomes to private variable

--- src/app/shared/utilities/outcome.utils.ts
+++ src/app/shared/utilities/outcome.utils.ts
@@ -3,8 +3,10 @@ import { OutcomeType } from '../dtos';
 
 @Injectable({ providedIn: 'root' })
 export class OutcomeUtils {
-    getOutcomes(): OutcomeType[] {
-        return [
+    outcomes: OutcomeType[];
+
+    constructor() {
+        this.outcomes = [
             new OutcomeType(1, 'Children have a strong sense of identity'),
             new OutcomeType(2, 'Children are connected with and contribute to their world'),
             new OutcomeType(3, 'Children have a strong sense of wellbeing'),
@@ -13,7 +15,11 @@ export class OutcomeUtils {
         ];
     }
 
+    getOutcomes(): OutcomeType[] {
+        return this.outcomes;
+    }
+
     getOutcomeDescription(id: number): string {
-        return this.getOutcomes().find(o => o.id === id).description || 'Unknown';
+        return this.outcomes.find(o => o.id === id).description || 'Unknown';
     }
 }
