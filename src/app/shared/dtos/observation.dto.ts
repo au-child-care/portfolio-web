c36c4a37ae4091525d72a44656d9ab237946a412
Angelito Sardez, Jr <jojosardez@gmail.com>
2019-04-20 14:40:31 +1000

    Added assessment and followup for observations

--- src/app/shared/dtos/observation.dto.ts
+++ src/app/shared/dtos/observation.dto.ts
@@ -4,6 +4,8 @@ export class Observation {
     child_id = 0;
     observation = '';
     outcome_id = 0;
+    assessment = '';
+    follow_up = '';
     date_tracked = '';
     published = 0;
     deleted = 0;
